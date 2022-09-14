46f5780ce7e9ae699c14c39a36d99371b2d872f3
Maximiliano Ortiz <49011550+maxiaortiz22@users.noreply.github.com>
2022-09-13 21:47:36 -0300

    Delete scripts directory

--- scripts/schroeder.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-def schroeder(ir, t, C):
-    """ Smooths a curve (ir) using Schroeder Integration method. "t" and "C" are Lundeby's compensation arguments """
-    ir = ir[0:int(t)]
-    y = np.flip((np.cumsum(np.flip(ir)) + C) / (np.sum(ir) + C))
-    return y
